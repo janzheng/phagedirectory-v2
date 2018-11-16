fc775c82b80c2cc30a4207ab0f9c60cbdafbf9d2
janzheng <janeazy@gmail.com>
2018-11-15 23:54:25 -0500

    super classy emails

--- components/EmailPeriodical.vue
+++ components/EmailPeriodical.vue
@@ -7,52 +7,54 @@
  -->
 <template>
 
-  <section class="Periodical narrow copy _margin-center _padding-top-2">
+  <section class="Email">
 
-    <div class="_card _padding-2" v-for="issue of issues" :key="issue.id" v-if="(showPreview && issue.fields.isPreview) || issue.fields.isPublished">
-      <!-- <div class="Periodical-header _grid-2">
-        <div class="Periodical-title">{{ issue.fields['Name'] }}</div>
-        <div class="Periodical-date _right-sm">{{ issue.fields['Date'] | niceDate }}</div>
-      </div> -->
-      <div class="Periodical-description _margin-bottom-2" v-html="$md.render(issue.fields['Markdown'] || '')"></div>
+    <div  v-for="issue of issues" :key="issue.id" v-if="(showPreview && issue.fields.isPreview) || issue.fields.isPublished">
 
-      <!-- twitter share on top -->
-      <div class="Periodical-share" v-if="issue.fields['TwitterText']">
-        <p class="Periodical-twitter">
-          <img src="https://abs.twimg.com/errors/logo23x19@2x.png" width="23px" height="19px" >&nbsp;
-          <a :href="`https://twitter.com/intent/tweet?text=${issue.fields['TwitterText']}`" >Tweet this issue!</a>
-        </p>
+      <div style="box-shadow: 0px 4px 8px rgba(70, 70, 70, .1); color: #333333; background-color: #FCFCFC; border-radius: 4px; padding: 30px; margin-bottom: 15px;">
+        <div class="Periodical-description _margin-bottom-2" v-html="$md.render(issue.fields['Markdown'] || '')"></div>
+
+        <!-- twitter share on top -->
+        <div class="Periodical-share" v-if="issue.fields['TwitterText']">
+          <p class="Periodical-twitter">
+            <img src="https://abs.twimg.com/errors/logo23x19@2x.png" width="23px" height="19px" >&nbsp;
+            <a :href="`https://twitter.com/intent/tweet?text=${issue.fields['TwitterText']}`" >Tweet this issue!</a>
+          </p>
+        </div>
       </div>
 
       <br/><br/>
 
-      <div class="Periodical-updates" v-if="getUpdates(issue)">
+      <div class="Periodical-updates" v-if="getUpdates(issue)" style="box-shadow: 0px 4px 8px rgba(70, 70, 70, .1); color: #333333; background-color: #FCFCFC; border-radius: 4px; padding: 30px; margin-bottom: 15px;">
         <h3 class="Periodical-updates-title_">{{issue.fields['UpdatesTitle'] || 'What’s New'}}<br/>
         <span style="color:#fa5486">&mdash;</span></h3>
         <div class="Periodical-update-item _margin-bottom" v-for="update of getUpdates(issue)" :key="update.fields['Name']" v-if="update && update.fields['isPublished']">
           <div class="_md-p_fix" v-html="$md.render(update.fields['Markdown'] || '')"></div>
           <div class="_margin-top-half" v-if="update.fields['Tags']">
             <span v-for="tag of update.fields.Tags" :key="tag"><span style="background-color:#eeeeee; border-radius:4px; border:1px solid #eeeeee; color:#000000; display:inline-block; font-size:12px; outline:none!important; padding:1px 8px; text-decoration:none" :class="tag == 'Sponsor' ? '--sponsor' : ''" >{{ tag }}</span>&nbsp;</span>
-          </div>
+          </div><br/>
         </div>
+        <br/>
         <hr style="border-color: #fa5486; border-width: 2px; border-style: solid" />
-
       </div>
 
-      <div class="Periodical-content" v-if="issue.fields['ArticleContent']" v-html="$md.render(issue.fields['ArticleContent'] || '')">
-      </div>
-
-      <!-- list has been moved to PeriodicalList.vue -->
-      
       <br/><br/>
 
-      <!-- twitter share on bottom -->
-      <div class="Periodical-share" v-if="issue.fields['TwitterText']">
-        <p class="Periodical-twitter">
-          <img src="https://abs.twimg.com/errors/logo23x19@2x.png" width="23px" height="19px" >&nbsp;
-          <a :href="`https://twitter.com/intent/tweet?text=${issue.fields['TwitterText']}`" >Tweet this issue!</a>
-        </p>
+      <div class="Periodical-content" v-if="issue.fields['ArticleContent']" style="box-shadow: 0px 4px 8px rgba(70, 70, 70, .1); color: #333333; background-color: #FCFCFC; border-radius: 4px; padding: 30px; margin-bottom: 15px;">
+        <div v-html="$md.render(issue.fields['ArticleContent'] || '')" >
+        </div>
+
+        <br/><br/>
+
+        <!-- twitter share on bottom -->
+        <div class="Periodical-share" v-if="issue.fields['TwitterText']">
+          <p class="Periodical-twitter">
+            <img src="https://abs.twimg.com/errors/logo23x19@2x.png" width="23px" height="19px" >&nbsp;
+            <a :href="`https://twitter.com/intent/tweet?text=${issue.fields['TwitterText']}`" >Tweet this issue!</a>
+          </p>
+        </div>
       </div>
+
 
     </div>
 
